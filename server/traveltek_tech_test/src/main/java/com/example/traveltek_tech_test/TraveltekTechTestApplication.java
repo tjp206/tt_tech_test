6bded760e4980949011c2ec664cf7d79a6384406
TJ <tjpataria@gmail.com>
2021-05-10 14:38:38 +0100

    initial commit

---
+++ server/traveltek_tech_test/src/main/java/com/example/traveltek_tech_test/TraveltekTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.traveltek_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltekTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltekTechTestApplication.class, args);
+	}
+
+}
